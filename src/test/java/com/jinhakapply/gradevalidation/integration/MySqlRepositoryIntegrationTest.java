b8872fab2b2ba798fc5f26f19fb2a2f9f9a166d4
janoogwan39 <janoogwan39@gmail.com>
2026-07-24 10:06:09 +0900

    fix: Flyway 마이그레이션 검증 버전 갱신

--- src/test/java/com/jinhakapply/gradevalidation/integration/MySqlRepositoryIntegrationTest.java
+++ src/test/java/com/jinhakapply/gradevalidation/integration/MySqlRepositoryIntegrationTest.java
@@ -109,7 +109,7 @@ class MySqlRepositoryIntegrationTest {
 
         assertThat(appliedVersions).containsExactly(
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
-            "17", "18", "19", "20", "21", "22", "23", "24", "25"
+            "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"
         );
         assertThat(statusDefault).isEqualTo("DRAFT");
         assertThat(legacySummaryUniqueColumns).containsExactly(
